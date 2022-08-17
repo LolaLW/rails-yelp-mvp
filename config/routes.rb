aa269f8833a155f294f134559e6b014762b1274f
Lolita Lugand <lyanlola77@gmail.com>
2022-08-17 12:19:35 +0200

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #  get '/restaurants/top', to: 'restaurants#top'
+  resources :restaurants do
+    collection do
+      get :top # restaurants/top
+    end
+    # Defines the root path route ("/")
+    # root "articles#index"
+  end
 end
